991007e5a729c53fac5fb0949aeec469cf55ac31
= <=>
2026-05-15 01:23:46 +0530

    File operation with command line input

--- file_op.js
+++ file_op.js
@@ -1,5 +1,4 @@
-
-const fs = require('fs');
+const fs = require("fs");
 
 //to create a file in sync
 // fs.writeFileSync('files/fruit.txt','I am a fruit');
@@ -14,3 +13,28 @@ const fs = require('fs');
 // const data = fs.readFileSync('files/fruit.txt', 'utf-8');
 // console.log(data);
 
+//file operations using command input
+let operation = process.argv[2]
+let filename = "files/" + process.argv[3] + ".txt"
+let content = process.argv[4]
+switch (operation) {
+  case "write":
+    fs.writeFileSync(filename, content);
+    break;
+  case "read":
+    let data = fs.readFileSync(filename, "utf-8")
+    console.log(data);
+    break
+  case "update":
+    fs.appendFileSync(filename, content)
+    break
+
+  case "delete":
+    fs.unlinkSync(filename)
+    console.log("File deleted Successfully")
+    break
+
+  default:
+    console.log("Invalid operation");
+    break
+}
