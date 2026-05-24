96c5d2f44fb6ee5335ea1b927c42dc8ccaba35f9
= <=>
2026-05-24 16:39:01 +0530

    Api using mongoose

--- express_js/apiUsingMongoose.js
+++ express_js/apiUsingMongoose.js
@@ -5,7 +5,7 @@ import studentModel from "./model/studentModel.js"
 const app = express()
 
 await mongoose.connect(url+'/school').then()
-
+app.use(express.json())
 app.get("/",(req,res)=>{
     res.send({})
 })
@@ -14,5 +14,31 @@ app.get("/",(req,res)=>{
 app.get("/students",async (req,res)=>{
     const studentData = await studentModel.find()
     res.send(studentData)
+})
+
+//api using mongoose to save data
+app.post("/save",async (req,res)=>{
+    const {name,age,email} = req.body
+    if(!req.body || !name || !age || !email)
+    {
+        res.send({message:"Operation Failed",success:false})
+        return false
+    }
+    const result = await studentModel.create(req.body)
+    res.send({message:"Operation Successful",success:true,data:result})
+})
+
+//put api to update data using mongoose
+app.put('/update/:id',async (req,res)=>{
+    const id = req.params.id
+    const data = await studentModel.findByIdAndUpdate(id,req.body)
+    res.send({message:"Update successful",success:true, data:data})
+})
+
+//delete api to update data using mongoose
+app.delete('/delete/:id',async (req,res)=>{
+    const id = req.params.id
+    const data = await studentModel.findByIdAndDelete(id)
+    res.send({message:"Delete successful",success:true, data:data})
 })
 app.listen(3200)
